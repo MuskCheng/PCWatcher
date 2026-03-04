2649ae95b155ac8e28a7ae5d8107547800de8655
MuskCheng <chengxinfei@gmail.com>
2026-03-04 10:25:33 +0800

    移除无价值的测试目录

--- tests/test_notifier.py
+++
@@ -1,4 +0,0 @@
-def test_pushme_client_init():
-    from notifier import PushMeClient
-    client = PushMeClient("test_key")
-    assert client.push_key == "test_key"
--- tests/test_tray.py
+++
@@ -1,5 +0,0 @@
-def test_tray_icon_creation():
-    from tray import create_icon_image
-    image = create_icon_image("green")
-    assert image is not None
-    assert image.size == (64, 64)
